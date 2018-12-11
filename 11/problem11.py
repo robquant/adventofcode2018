fbfdc087a3d6c549d2aaae5e6d4f13e0c7502862
Robert Franke <franke.rob@gmail.com>
2018-12-11 21:43:42 +0100

    Problem 11 part 1

---
+++ 11/problem11.py
@@ -0,0 +1,67 @@
+
+    # Find the fuel cell's rack ID, which is its X coordinate plus 10.
+    # Begin with a power level of the rack ID times the Y coordinate.
+    # Increase the power level by the value of the grid serial number (your puzzle input).
+    # Set the power level to itself multiplied by the rack ID.
+    # Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+    # Subtract 5 from the power level.
+
+
+# (3, 5) 8 -> 4
+
+def power(coords, serial):
+    x, y = coords
+    cell_rack_id = x + 10
+    power_level = cell_rack_id * y
+    power_level += serial
+    power_level *= cell_rack_id
+    power_level = power_level // 100
+    power_level = power_level % 10
+    return power_level - 5
+
+def test_power():
+    assert power((122,79), 57) == -5
+    assert power((217,196), 39) == 0
+    assert power((101,153), 71) == 4
+
+def max_rectangle(field, size):
+    size_x, size_y = size
+    max_sum = -9999
+    max_coords = None
+    for x in range(0, 301 - size_x):
+        for y in range(0, 301 - size_y):
+            s = 0
+            for i in range(x, x + size_x):
+                for j in range(y, y + size_y):
+                    s += field[j][i]
+            if s > max_sum:
+                max_sum = s
+                max_coords = (x, y)
+    return max_coords, max_sum
+
+def fill_field(serial):
+    field = [[0] * 300 for _ in range(300)]
+    for x in range(1, 301):
+        for y in range(1, 301):
+            field[y-1][x-1] = power((x,y), serial)
+    return field
+
+if __name__ == "__main__":
+    serial = 2187
+    field = fill_field(serial)
+    coords, max_sum = max_rectangle(field, (3,3))
+    x,y = coords
+    print(x+1, y+1)
+
+    max_coords = None
+    max_sum = -9999
+    rect_size = None
+    for size in range(1, 301):
+        print(size)
+        coords, sum = max_rectangle(field, (size, size))
+        if sum > max_sum:
+            max_sum = sum
+            max_coords = coords
+            rect_size = size
+    x,y = max_coords
+    print(x+1, y+1, rect_size)
